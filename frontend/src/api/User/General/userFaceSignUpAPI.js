d2ba62ae1ca19183b311e55c5278a1b1b88c47aa
Lu-weiting <runeeld23@gmail.com>
2024-11-01 17:00:33 +0800

    Feat: frontend

--- frontend/src/api/User/General/userFaceSignUpAPI.js
+++ frontend/src/api/User/General/userFaceSignUpAPI.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 // const api = import.meta.env.VITE_REACT_APP_API;
-const api = `http://140.119.19.85:80/api/1.0`;
-const apiUrl = `${api}/user/faceSignup`;
+// const api = `http://140.119.19.85:80/api/1.0`;
+const apiUrl = `/face/register`;
 
 export async function userFaceSignUpAPI(body) {
   try {
